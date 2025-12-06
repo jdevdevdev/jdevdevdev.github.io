61356132dcfc277beb36b97c0831bc69ffc3e4ec
jdevdevdev <117501493+jdevdevdev@users.noreply.github.com>
2025-12-07 02:54:21 +1100

    Update next.js remove qr testing

--- src/app/qr-file-reader/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import PdfViewer from "./PdfViewer";
-
-export default function Page() {
-  return (<div>
-    <PdfViewer fileUrl={"/qr-test3.pdf"}></PdfViewer>
-  </div>);
-}
